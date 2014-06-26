4384727e4687d519f2a68514d34c5efe5581c07a
Roberto <rfjunior@br.ibm.com>
2014-06-26 18:51:07 -0300

    my django app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn backups_app.wsgi
---
+++ venv_backups_app/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rfjunior/workspace/backups_app_git_root/venv_backups_app/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
